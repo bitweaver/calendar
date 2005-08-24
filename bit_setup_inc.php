f8c6c98b01d73d1a0a2f32fd345a7a7b50745c95
Max Kremmel <xing@synapse.plus.com>
2005-08-24 22:45:54 +0000

    display menu only when user has perm to use it

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,7 +3,9 @@ global $gBitSystem, $gBitSmarty;
 $gBitSystem->registerPackage( 'calendar', dirname( __FILE__).'/' );
 
 if( $gBitSystem->isPackageActive( 'calendar' ) ) {
-	$gBitSystem->registerAppMenu( 'calendar', 'Calendar', CALENDAR_PKG_URL.'index.php', 'bitpackage:calendar/menu_calendar.tpl', 'calendar');
+	if( $gBitUser->hasPermission( 'bit_p_view_calendar' ) ) {
+		$gBitSystem->registerAppMenu( 'calendar', 'Calendar', CALENDAR_PKG_URL.'index.php', 'bitpackage:calendar/menu_calendar.tpl', 'calendar');
+	}
 }
 
 ?>
