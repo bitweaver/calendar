5326fb2cf4bdf08d18b869693a76569ba1602fb1
Max Kremmel <xing@synapse.plus.com>
2006-01-17 20:13:46 +0000

    use PKG_NAME for app menu key and dir name for menu title - needed for icons on 'my' page

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,7 +4,7 @@ $gBitSystem->registerPackage( 'calendar', dirname( __FILE__).'/' );
 
 if( $gBitSystem->isPackageActive( 'calendar' ) ) {
 	if( $gBitUser->hasPermission( 'bit_p_view_calendar' ) ) {
-		$gBitSystem->registerAppMenu( CALENDAR_PKG_DIR, 'Calendar', CALENDAR_PKG_URL.'index.php', 'bitpackage:calendar/menu_calendar.tpl', CALENDAR_PKG_NAME );
+		$gBitSystem->registerAppMenu( CALENDAR_PKG_NAME, ucfirst( CALENDAR_PKG_DIR ), CALENDAR_PKG_URL.'index.php', 'bitpackage:calendar/menu_calendar.tpl', CALENDAR_PKG_NAME );
 	}
 }
 
