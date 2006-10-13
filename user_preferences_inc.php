548fcc5c5ade6cd5191694e831e7d0028a866e65
Lester Caine <lester@lsces.co.uk>
2006-10-13 12:42:50 +0000

    phpDoc corrections

--- user_preferences_inc.php
+++ user_preferences_inc.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_calendar/user_preferences_inc.php,v 1.2 2006/10/13 12:42:50 lsces Exp $
+ * @package calendar
+ */
+
+/**
+ * Setup
+ */
 if( $gBitSystem->isFeatureActive('calendar_user_prefs') ) {
 	include_once( CALENDAR_PKG_PATH.'admin/admin_calendar_inc.php' );
 	if( !empty( $_REQUEST['calendar_submit'] ) ) {
