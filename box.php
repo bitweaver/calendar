c86f1ecd6a6b690b105cf9a67a0c40e6c027577b
Nick Palmer <nick@sluggardy.net>
2007-04-05 21:33:07 +0000

    Fix display of content description in ajax popup.

--- box.php
+++ box.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_calendar/box.php,v 1.1 2007/04/05 21:20:59 nickpalmer Exp $
+// $Header: /cvsroot/bitweaver/_bit_calendar/box.php,v 1.2 2007/04/05 21:33:07 nickpalmer Exp $
 
 // Copyright( c ) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -8,6 +8,10 @@
 require_once( '../bit_setup_inc.php' );
 
 include_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
+
+if (!empty($gContent->mInfo['content_type_guid'])) {
+	$gContent->mInfo['content_description'] = $gLibertySystem->mContentTypes[$gContent->mInfo['content_type_guid']]['content_description'];
+}
 
 $gBitSmarty->assign('cellHash', $gContent->mInfo);
 
