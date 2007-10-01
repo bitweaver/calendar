93de57dff06121af9efa89c624f4257b587cdb82
Nick Palmer <nick@sluggardy.net>
2007-10-01 16:08:03 +0000

    Fix mincal so it works even when data.calendar plugin is off.

--- modules/mod_minical.php
+++ modules/mod_minical.php
@@ -1,5 +1,7 @@
 <?php
 global $gBitSmarty;
 
-$gBitSmarty->assign('mini_cal',data_calendar('',''));
+// Make sure we know how to do the data_calendar
+require_once(LIBERTY_PKG_PATH.'plugins/data.calendar.php');
+$gBitSmarty->assign( 'mini_cal',data_calendar( '', '' ) );
 ?>
