5fd474f76f457b04d516b87475a582b5c24ba942
wjames5 <will@tekimaki.com>
2008-07-23 12:47:17 +0000

    point to new location of plugin

--- modules/mod_minical.php
+++ modules/mod_minical.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_calendar/modules/mod_minical.php,v 1.4 2008/02/10 12:18:52 nickpalmer Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_calendar/modules/mod_minical.php,v 1.5 2008/07/23 12:47:17 wjames5 Exp $
  * @package calendar
  * @subpackage modules
  */
@@ -11,6 +11,6 @@
 global $gBitSmarty;
 
 // Make sure we know how to do the data_calendar
-require_once(LIBERTY_PKG_PATH.'plugins/data.calendar.php');
+require_once(CALENDAR_PKG_PATH.'liberty_plugins/data.calendar.php');
 $gBitSmarty->assign( 'mini_cal',data_calendar( '', empty($moduleParams['module_params']) ? NULL : $moduleParams['module_params'] ) );
 ?>
