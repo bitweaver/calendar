43663f76688a624b0a113516a5f37184a44dc474
lsces <lester@lsces.co.uk>
2013-07-12 23:22:32 +0100

    Closing bracket missmatch fixed

--- modules/mod_minical.php
+++ modules/mod_minical.php
@@ -12,5 +12,5 @@ global $gBitSmarty;
 
 // Make sure we know how to do the data_calendar
 require_once(CALENDAR_PKG_PATH.'liberty_plugins/data.calendar.php');
-$_template->tpl_vars['mini_cal'] = new Smarty_variable(data_calendar( '', empty($moduleParams['module_params']);
+$_template->tpl_vars['mini_cal'] = new Smarty_variable(data_calendar( '', empty($moduleParams['module_params']) ) );
 ?>
