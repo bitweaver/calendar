001c8743879bf698260bdc3203c1367b8816b368
modela bitweaver <spiderr@bitweaver.org>
2021-04-15 12:14:53 -0400

    clean up for includes/ reorg

--- includes/bit_setup_inc.php
+++ includes/bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem, $gBitSmarty, $gBitUser;
 
 $registerHash = array(
 	'package_name' => 'calendar',
-	'package_path' => dirname( __FILE__ ).'/',
+	'package_path' => dirname( dirname( __FILE__ ) ).'/',
 	'homeable' => TRUE,
 );
 $gBitSystem->registerPackage( $registerHash );
